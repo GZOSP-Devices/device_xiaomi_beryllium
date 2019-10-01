afc2c8a6d63009f100e7cc08b94aab165c995bd8
Lup Gabriel <gwolf2u.website@gmail.com>
2019-10-01 20:07:53 +0300

    beryllium: just let me pass SafetyNet CTS

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -23,4 +23,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="beryllium" \
     TARGET_DEVICE="beryllium"
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
